a70a4f0eefa6628c29a80f4bda7cc793bf4040cf
Marcus Medin <medina.jobb@outlook.com>
2022-09-06 10:46:17 +0200

    Add project files.

---
+++ CLO22InputSiffror/Program.cs
@@ -0,0 +1,13 @@
+﻿// Heltal = int = int32
+
+
+// Deklarera och tilldela värdet 0
+int age = 0;
+
+Console.WriteLine("Ange din ålder");
+string input = Console.ReadLine();
+
+age = int.Parse(input);
+float days = 365.25f * age;
+Console.WriteLine("Du har levt " + days + " dagar");
+
